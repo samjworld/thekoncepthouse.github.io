fb8348eb298733d9da457a761c09d0a8f4e50075
samjworld <sam.jworld@gmail.com>
2021-09-05 12:38:29 +0530

    asdf

--- assets/contact.php
+++ assets/contact.php
@@ -7,7 +7,7 @@ $message= $_POST['message'];
 $phone= $_POST['phone'];
 $to = "subhrangsupal@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message. "\r\n Phone number =". $phone;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message. "\r\n Phone =". $phone;
 $headers = "From: noreply@thekoncepthouse.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
